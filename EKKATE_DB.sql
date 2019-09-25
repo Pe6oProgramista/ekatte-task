8352d389a1c5f6eff96db0357d512e065dd64739
Pe6oProgramista <p.milev3@gmail.com>
2019-09-25 18:58:24 +0300

    fiter data from excel files

--- EKKATE_DB.sql
+++ EKKATE_DB.sql
@@ -5,28 +5,22 @@ CREATE DATABASE ekatte;
 CREATE TABLE "oblast" (
   "id" SERIAL PRIMARY KEY UNIQUE NOT NULL,
   "oblast" varchar UNIQUE,
-  "name" varchar,
-  "center" varchar,
-  "region" varchar
+  "name" varchar
 );
 
 CREATE TABLE "obstina" (
   "id" SERIAL PRIMARY KEY UNIQUE NOT NULL,
   "obstina" varchar UNIQUE,
   "name" varchar,
-  "oblast" varchar,
-  "center" varchar,
-  "category" varchar
+  "oblast" varchar
 );
 
 CREATE TABLE "atte" (
   "id" SERIAL PRIMARY KEY UNIQUE NOT NULL,
-  "ekatte" varchar UNIQUE,
+  "ekatte" varchar,
   "name" varchar,
   "obstina" varchar,
-  "kind" varchar,
-  "category" varchar,
-  "altitude" varchar
+  "kind" varchar
 );
 
 CREATE TABLE "kind" (
@@ -34,11 +28,7 @@ CREATE TABLE "kind" (
   "kind" varchar UNIQUE,
   "tvm" varchar
 );
-
-ALTER TABLE "oblast" ADD FOREIGN KEY ("center") REFERENCES "atte" ("ekatte");
-
 ALTER TABLE "obstina" ADD FOREIGN KEY ("oblast") REFERENCES "oblast" ("oblast");
-ALTER TABLE "obstina" ADD FOREIGN KEY ("center") REFERENCES "atte" ("ekatte");
 
 ALTER TABLE "atte" ADD FOREIGN KEY ("obstina") REFERENCES "obstina" ("obstina");
 ALTER TABLE "atte" ADD FOREIGN KEY ("kind") REFERENCES "kind" ("kind");
--- importData.js
+++ importData.js
@@ -1,20 +1,52 @@
 const { Client } = require('pg');
+const XLSX = require('xlsx');
 
 const client = new Client({
-  user: process.env.DB_USER,
-  host: 'localhost',
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: 5432,
+	user: process.env.DB_USER,
+	host: 'localhost',
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASS,
+	port: 5432,
 });
 
 async function main() {
-  await client.connect();
-  const res = await client.query('SELECT NOW()');
-  await client.end();
-  
-  console.log(res)
-  return 0;
+	await client.connect();
+	const res = await client.query('SELECT NOW()');
+	await client.end();
+
+	let workbook = XLSX.readFile('./files/Ek_atte.xlsx', { sheetRows: 4 }); // , { sheetRows: 3 }
+	let sheetsList = workbook.SheetNames;
+	let atteData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetsList[0]], {
+		header: 1,
+		defval: '',
+		blankrows: true
+	});
+	let keys = atteData[0];
+	atteData.splice(0, 2);
+
+	atteData = atteData.map((row) => {
+		return Object.assign({}, ...row
+			.map((v, i) => {
+				return { [keys[i]]: v };
+			}));
+	});
+
+	let kindData = atteData.map( (row) => {
+		return { kind: row['kind'], t_V_m: row['t_v_m'] } 
+	});
+
+	atteData = atteData.map( (row) => {
+		return {
+			ekatte: row['ekatte'],
+			name: row['name'],
+			obstina: row['obstina'],
+			kind: row['kind']
+		}
+	} )
+
+	console.log(atteData);
+
+	return 0;
 }
 
 main();
